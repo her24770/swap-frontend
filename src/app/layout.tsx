b02e97ebdccd35a2accff54d1ad854ab3de7b4f2
Alemanuel <alemanuelj5@gmail.com>
2026-04-30 09:28:05 -0600

    Integracion de las Toast en layout, modificacion del nombre de la pagina y su descripcion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
 import Layout from "../components/layout/Layout/layout"
+import ToastContainer from "../components/ui/Toast/Toast"
 import "./globals.css"
 import { jockey, palanquin, lato} from "../styles/fonts";
 
 export const metadata = {
-  title: 'Mi App en Docker',
-  description: 'Generado por Next.js',
+  title: 'Swap',
+  description: 'Sitio de intercambio enttre estudiantes de la UVG',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout(
+  {children,}: 
+  {children: React.ReactNode}) {
   return (
     <html lang="es" className={`${jockey.variable} ${palanquin.variable} ${lato.variable}`}>
       <body>
         <Layout>{children}</Layout>
+        <ToastContainer />
       </body>
     </html>
   )
